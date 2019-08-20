0ec26376b728cdc2e20f951f3b5ba5ec6f5c07de
John Odom <john.osborne.odom@gmail.com>
2019-08-20 17:16:21 -0400

    Small Changes - stuck on Outfits Edit Page

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -1,19 +1,25 @@
 class OutfitsController < ApplicationController
-    before_action :authorize!, only:[:new]
+    # before_action :authorize!, only:[:new]
 def index
     @outfits = Outfit.all
 end
 def show
-    @outfit = Outfit.find_by_id(params[:id])
+    @outfit = Outfit.find(params[:id])
 end
 def new
     @outfit = Outfit.new
-    @items = Item.all 
-    @outfit_items = OutfitItem.all 
 end
 
 def create
+    @outfit = Outfit.create(params.require(:outfit).permit(:name))
+    redirect_to outfit_path(@outfit)
+end
 
+def edit
+    @outfit = Outfit.find(params[:id])
+end
+
+def update
 end
 
 end
