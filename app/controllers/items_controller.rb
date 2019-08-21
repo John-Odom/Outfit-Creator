afc6104d4ef3ba536b5774a506430529fcec90c2
John Odom <john.osborne.odom@gmail.com>
2019-08-21 16:20:45 -0400

    Bus squashed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+    helper_method :uniq_users
+    
     def index
         @items = Item.all 
     end
@@ -6,5 +8,14 @@ class ItemsController < ApplicationController
     def show
         @item = Item.find(params[:id])
     end
+
+    def uniq_users
+        array = [] 
+        @item = Item.find(params[:id])
+        @item.outfits.each do |o|
+          array << o.user.to_s
+        end
+        array = array.uniq
+      end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ApplicationRecord
   belongs_to :style, required: false 
   has_many :outfit_items
   has_many :outfits, through: :outfit_items 
+
+ 
+
 end
